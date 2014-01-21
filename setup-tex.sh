fbefe524bb2b10970bdfd4fab5a77de0215aeb25
Michael George <mdgeorge@cs.cornell.edu>
2014-01-20 22:35:14 -0500

    just install the stupid fonts

--- setup-tex.sh
+++ setup-tex.sh
@@ -1,11 +1,9 @@
 sudo apt-get install -y --no-install-recommends \
-  texlive-latex-base texlive-latex-extra texlive-fonts-recommended pgf xpdf
+  texlive-latex-base texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra pgf xpdf
 
 [ -d 3110-tools ] || git clone https://github.com/cs3110/tools.git 3110-tools
 
 # install tex stuff
 sudo cp -rT 3110-tools/texmf /usr/local/share/texmf
-sudo mktexlsr
-sudo updmap-sys Map=dejavu-type1.map
 sudo mktexlsr
 
