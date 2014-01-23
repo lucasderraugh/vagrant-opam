eb1a46b4bb57cffb0d3803836bd5bac82dd32935
bkc39 <bkc39@cornell.edu>
2014-01-23 14:32:45 -0500

    Check for .emacs.d diretory before making it

--- setup-emacs.sh
+++ setup-emacs.sh
@@ -6,5 +6,5 @@ sudo apt-get install -y emacs
 export OPAMYES=1
 opam install ocp-index ocp-indent
 [ -d 3110-tools ] || git clone git@github.com:cs3110/tools.git 3110-tools
-mv 3110-tools/.emacs.d/ $HOME/.emacs.d/
+[ -d 3110-tools ] || mv 3110-tools/.emacs.d/ $HOME/.emacs.d/
 echo 'Emacs successfully installed'
