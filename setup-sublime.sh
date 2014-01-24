6f049a47ebaf9a271ab6ca93ea08af5f7e118b90
bkc39 <bkc39@cornell.edu>
2014-01-23 19:58:05 -0500

    Fix the apt repo that we pull

--- setup-sublime.sh
+++ setup-sublime.sh
@@ -3,9 +3,9 @@
 set -ex
 
 ## Download and install Sublime
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install sublime-text-installer
 
 ## Add the alias to the bashrc
 echo "alias sublime=sublime-text-3" >> $HOME/.bashrc
