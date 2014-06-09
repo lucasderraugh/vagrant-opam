dffc4a07609ddb5c7dfd6efff4b67772373ec207
bkc39 <bkc39@cornell.edu>
2014-06-09 15:40:04 -0400

    Change to download Emacs 24.3

--- setup-emacs.sh
+++ setup-emacs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
-sudo apt-get install -y emacs
+## Installs Emacs 24 (apt currently has 23 by default)
+sudo add-apt-repository ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install emacs24 emacs24-e1 emacs24-common-non-dfsg
 
 export OPAMYES=1
 opam install ocp-index ocp-indent
 [ -d 3110-tools ] || git clone git@github.com:cs3110/tools.git 3110-tools
-[ -d 3110-tools ] || cp -rT 3110-tools/.emacs.d/ $HOME/.emacs.d/
+[ -d 3110-tools ] || cp -rT 3110-tools/emacs/ $HOME/.emacs.d/
 echo 'Emacs successfully installed'
