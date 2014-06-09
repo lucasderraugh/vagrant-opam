8f28a986253debd748448f63cc8b6c40e99d0668
bkc39 <bkc39@cornell.edu>
2014-06-09 18:29:35 -0400

    Fix the emacs setup.

--- setup-emacs.sh
+++ setup-emacs.sh
@@ -4,10 +4,10 @@ set -ex
 ## Installs Emacs 24 (apt currently has 23 by default)
 sudo add-apt-repository ppa:cassou/emacs
 sudo apt-get update
-sudo apt-get install emacs24 emacs24-e1 emacs24-common-non-dfsg
+sudo apt-get install emacs24 emacs24-el emacs24-common-non-dfsg -y
 
 export OPAMYES=1
 opam install ocp-index ocp-indent
 [ -d 3110-tools ] || git clone git@github.com:cs3110/tools.git 3110-tools
-[ -d 3110-tools ] || cp -rT 3110-tools/emacs/ $HOME/.emacs.d/
+cp -rT 3110-tools/emacs/ $HOME/.emacs.d/
 echo 'Emacs successfully installed'
