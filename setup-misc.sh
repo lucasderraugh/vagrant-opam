47678843c7e1dee1d04801761c733f9f24ea9bb3
Remy Jette <remy@remyjette.com>
2015-08-11 12:22:46 -0400

    Disable the screensaver in the VM

--- setup-misc.sh
+++ setup-misc.sh
@@ -3,3 +3,6 @@
 #set up timezone
 echo "America/New_York" | sudo tee /etc/timezone
 sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+#disable the screensaver
+sudo apt-get remove -y xscreensaver
