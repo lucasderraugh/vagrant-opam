3474797f77045cc2042876d7df2a4818216b43e7
Remy Jette <remy@remyjette.com>
2015-08-11 13:22:24 -0400

    Don't remove the vagrant password, so people can still log in as vagrant/vagrant

--- setup-lxde.sh
+++ setup-lxde.sh
@@ -8,7 +8,6 @@ sudo /usr/share/debconf/fix_db.pl
 sudo apt-get install -y lxde chromium-browser
 
 sudo sed "s/# autologin=dgod/autologin=vagrant/" -i /etc/lxdm/lxdm.conf
-sudo passwd -d vagrant
 
 sudo apt-get install -y chromium-browser
 
