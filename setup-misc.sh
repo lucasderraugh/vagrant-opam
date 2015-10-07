3191d418b5a46774abf08a78aaf6269bcecbc88e
Remy Jette <remy@remyjette.com>
2015-10-06 21:32:34 -0400

    Copy bash config file from the tools repo during setup

--- setup-misc.sh
+++ setup-misc.sh
@@ -7,6 +7,9 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 #disable the screensaver
 sudo apt-get remove -y xscreensaver
 
+#copy .bashrc bash config file
+cp 3110-tools/config/bashrc ~/.bashrc
+
 #copy .utoprc here rather than in setup-opam.sh to ensure the 3110-tools dir
 #is available
 cp 3110-tools/config/utoprc ~/.utoprc
